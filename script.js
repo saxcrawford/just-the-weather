5bf010a84d3a34930ac059549636967c8e1ad92f
Saxon <saxon.crawford@icloud.com>
2024-10-02 13:32:27 -0400

    Style changes/Added background images

--- script.js
+++ script.js
@@ -12,6 +12,42 @@ function fetchWeather(city) {
         .then((response) => response.json())
         .then((data) => {
             showWeather(data);
+            const attributionElement = document.getElementById('image-attribution');
+            const photographerLink = document.getElementById('photographer-link');
+            if (data.weather[0].main == 'Mist' || data.weather[0].main == 'Fog' || data.weather[0].main == 'Haze' || data.weather[0].main == 'Smoke' || data.weather[0].main == 'Dust' || data.weather[0].main == 'Sand' || data.weather[0].main == 'Ash' || data.weather[0].main == 'Squall' || data.weather[0].main == 'Tornado') {
+                document.body.style.backgroundImage = "url('images/mist.jpg')";
+                photographerLink.href = "https://unsplash.com/@sharadmbhat?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash";
+                photographerLink.textContent = "Sharad Bhat - unsplash";
+                attributionElement.style.display = 'block';
+            } else if (data.weather[0].main == 'Thunderstorm') {
+                document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+                photographerLink.href = "https://unsplash.com/@noaa?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash";
+                photographerLink.textContent = "NOAA - unsplash";
+                attributionElement.style.display = 'block';
+            } else if (data.weather[0].main == 'Rain' || data.weather[0].main == 'Drizzle') {
+                document.body.style.backgroundImage = "url('images/rain.jpg')";
+                photographerLink.href = "https://unsplash.com/@josenothose?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash";
+                photographerLink.textContent = "Jose Fontano - unsplash";
+                attributionElement.style.display = 'block';
+            } else if (data.weather[0].main == 'Snow') {
+                document.body.style.backgroundImage = "url('images/snow.jpg')";
+                photographerLink.href = "https://www.freepik.com/free-photo/small-coniferous-tree-field_1579320.htm#fromView=search&page=4&position=9&uuid=0f4d5c52-3c0e-4cb4-b68a-8bbcacc518df";
+                photographerLink.textContent = "freepik";
+                attributionElement.style.display = 'block';
+            } else if (data.weather[0].main == 'Clear') {
+                document.body.style.backgroundImage = "url('images/clear.jpg')";
+                photographerLink.href = "https://www.freepik.com/free-photo/beautiful-photo-sea-waves_179723483.htm#fromView=search&page=1&position=15&uuid=f4c4500e-b97f-43c7-8b24-1dbecc04f2c2";
+                photographerLink.textContent = "Mateus Andre - freepik";
+                attributionElement.style.display = 'block';
+            } else if (data.weather[0].main == 'Clouds') {
+                document.body.style.backgroundImage = "url('images/clouds.jpg')";
+                photographerLink.href = "https://unsplash.com/@marcwieland95?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash";
+                photographerLink.textContent = "Marc Wieland - unsplash";
+                attributionElement.style.display = 'block';
+            } else {
+                document.body.style.backgroundColor = '#B7E0FF';
+                attributionElement.style.display = 'none';
+            }
         })
         .catch((error) => {
             console.error('Error fetching weather data:', error);
